1d37797ffbbdfeaff20579899d1795f447ce4306
Iionsroar <yawned.eyes@gmail.com>
2022-08-19 23:29:22 +0800

    test layout

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Download the example [or clone the repo](https://github.com/mui/material-ui):
 <!-- #default-branch-switch -->
 
 ```sh
-curl https://codeload.github.com/mui/material-ui/tar.gz/master | tar -xz --strip=2  material-ui-master/examples/nextjs
+npx create-next-app example-app --use-npm --example "https://github.com/mui/material-ui/tree/master/examples/nextjs"
 cd nextjs
 ```
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Layout from './components/layout';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
+    <Layout>
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
@@ -19,6 +19,6 @@ export default function Index() {
         <ProTip />
         <Copyright />
       </Box>
-    </Container>
+    </Layout>
   );
 }
