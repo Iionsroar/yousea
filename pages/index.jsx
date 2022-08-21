84529a5752c8da5b4a57b260b8acaabcdf54de87
Iionsroar <yawned.eyes@gmail.com>
2022-08-21 16:47:23 +0800

    add rendered alerts from list; add function to handle submits

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,20 +8,53 @@ import IconButton from '@mui/material/IconButton';
 import Send from '@mui/icons-material/Send';
 import Layout, { siteTitle } from './components/layout';
 import Link from '../src/Link';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function Index() {
-  const [url, setUrl] = React.useState('http://url.com');
   const validUrl = /^(ftp|http|https):\/\/[^ "]+$/;
+  const [url, setUrl] = React.useState('http://url.com');
+  const [updates, setUpdates] = React.useState([]); 
+  // TODO: SEND delete request ONCLICK of undo button
+  const updateItems = updates.map(update =>
+      <Alert 
+        key={update}
+        action={
+          <Button color="inherit" size="small">
+            UNDO
+          </Button>
+        }
+      >
+        Added {update}
+      </Alert>
+  )
+
+  // TODO: SEND post request IF value IS VALID
+  const handleSubmit = (event) => {
+    const val = event.target.value;
+    setUrl(val);
+    if (validUrl.test(val)) {
+      if (!updates.includes(val)) {
+        let tempUpdates = updates;
+        tempUpdates.push(val);
+        setUpdates(tempUpdates);
+      }
+      event.target.value = '';
+    }
+  }
 
   return (
     <Layout home>
       <Head>
         <title>Home</title>
       </Head>    
+
       <Box align="center" sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           { siteTitle }
         </Typography>
+
         <TextField 
           id="url"
           InputProps={{
@@ -31,18 +64,9 @@ export default function Index() {
               </InputAdornment>
             )
           }}
-          error={!validUrl.test(url)}
+          error={ !validUrl.test(url) }
           helperText={!validUrl.test(url) ? "URL is not correct" : ""}
-          onBlur={(event) => {
-            setUrl(event.target.value)
-            if (validUrl.test(event.target.value)) {
-                // CLEAR textfield
-                // SEND post request IF value IS VALID
-                // SHOW alert
-                console.log('Link Posted!')
-              }
-            }
-          }
+          onBlur={handleSubmit}
           variant="standard"
           size="medium" 
           type="url"
@@ -50,6 +74,11 @@ export default function Index() {
           fullWidth 
         />
       </Box>
+
+      <Stack sx={{ width: '100%' }} spacing={1}>
+          { updateItems }
+      </Stack>
+
       <Box align="center" sx={{ my: 4 }}>
         <Link href="/about" color="secondary">
           Go to the about page
